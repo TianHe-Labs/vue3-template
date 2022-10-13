6fb1f318b64b33a410f0bfa1fefafe5fec0db943
Evant <rayshinng12@163.com>
2022-10-13 16:37:00 +0800

    docs: 添加注释

--- src/router/guards/title.ts
+++ src/router/guards/title.ts
@@ -3,13 +3,7 @@ import type { Router } from 'vue-router'
 export function createTitleGuard(router: Router) {
   router.beforeEach(async (to) => {
     // 更新 title
-    let subtitle = to.meta.title
-    for (const key of ['domain', 'ipv4', 'org']) {
-      if (to.params[key]) {
-        subtitle = to.params[key] as string
-        break
-      }
-    }
+    const subtitle = to.meta.title
     document.title = `${subtitle}丨${import.meta.env.VITE_APP_NAME}`
   })
 }
