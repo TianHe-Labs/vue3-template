316d64523a950a4f66ad39e151a3a0991af59d8f
Evant <rayshining12@163.com>
2023-03-23 17:31:33 +0800

    fix: 修复标题问题

--- src/router/guards/title.ts
+++ src/router/guards/title.ts
@@ -1,11 +1,13 @@
 import type { Router } from 'vue-router'
+import { useAppStore } from '@/store'
 
 export function createTitleGuard(router: Router) {
   router.beforeEach(async (to) => {
+    const { name: appName } = useAppStore()
     // 更新 title
     const subtitle = to.meta.title
     if (subtitle) {
-      document.title = `${subtitle}丨${document.title}`
+      document.title = `${subtitle}丨${appName}`
     }
   })
 }
