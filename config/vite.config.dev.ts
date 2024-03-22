ba87dfa99c2f6ff492fea2898591f0eb9d66c55b
Evant <rayshining12@163.com>
2024-03-22 17:31:31 +0800

    fix: 修复参数

--- config/vite.config.dev.ts
+++ config/vite.config.dev.ts
@@ -1,11 +1,9 @@
 import { mergeConfig } from 'vite'
 import baseConfig from './vite.config.base'
-import htmlPlugin from './plugins/html'
 import { createProxy } from './utils'
 
 export default mergeConfig(
   {
-    plugins: [htmlPlugin()],
     server: {
       open: false,
       host: true,
